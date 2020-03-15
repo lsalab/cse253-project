0b8e8b3531ff26b1e19a8ee6ec0983d406076aac
Neil_PC <nortizsi@ucsc.edu>
2020-03-14 20:26:52 -0700

    third commit

--- iec104.py
+++ iec104.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 
+def __init__(self, typeASDU, ioa):
+    self.typeASDU = typeASDU
+    self.ioa = ioa
+
+
+def pack(self, measurement):
+    pass
+
+def unPack(self, paquete):
+    pass
