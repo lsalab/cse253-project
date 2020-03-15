e7642afac2b87b594d9d07c96924d389d0eefbf9
Luis Salazar <lsalab@gmail.com>
2020-03-14 20:24:36 -0700

    Load sim

--- rtu.py
+++ rtu.py
@@ -70,9 +70,11 @@ class Transmission(RTU):
 
     def __init__(self, **kwargs):
         super(Transmission, self).__init__(**kwargs)
-        if any(k not in kwargs.keys() for k in ['state', 'states', 'loads', 'left', 'right']):
+        if any(k not in kwargs.keys() for k in ['state', 'loads', 'left', 'right']):
             raise AttributeError()
-        if any(not isinstance(kwargs[k], int) for k in ['state', 'left', 'right']) or any(not isinstance(kwargs[k], list) for k in ['states', 'loads']):
+        if any(not isinstance(kwargs[k], int) for k in ['state', 'left', 'right']) or not isinstance(kwargs['loads'], list):
+            raise AttributeError()
+        if any(not isinstance(k, float) for k in kwargs['loads']):
             raise AttributeError()
         self.__state = kwargs['state']
         self.__loads = kwargs['loads']
@@ -82,39 +84,77 @@ class Transmission(RTU):
         self.__vin = None
         self.__vout = None
         self.__amp = None
+        self.__rload = None
 
     @property
     def load(self) -> float:
         return self.__load
-
     @load.setter
     def load(self, value: float):
         self.__load = value
 
+    @property
+    def vin(self) -> float:
+        return self.__vin
+    @vin.setter
+    def vin(self, value: float):
+        self.__vin = value
+
+    @property
+    def vout(self) -> float:
+        return self.__vout
+    @vout.setter
+    def vout(self, value: float):
+        self.__vout = value
+    
+    @property
+    def rload(self) -> float:
+        return self.__rload
+    @rload.setter
+    def rload(self, value: float):
+        self.__rload = value
+    
+    @property
+    def amp(self) -> float:
+        return self.__amp
+    @amp.setter
+    def amp(self, value: float):
+        self.__amp = value
+
     def calculate_load(self):
-        self.__load = -1
+        self.__load = None
         for i in range(len(self.__loads)):
             if (state & (2 ** i)) > 0:
                 if self.__loads[i] == 0:
                     self.__load = 0 # Failure
                     return
-                self.__load = self.__loads[i] if self.__load == -1 else (self.__load * self.__loads[i]) / (self.__load + self.__loads[i])
+                self.__load = self.__loads[i] if self.__load is None else (self.__load * self.__loads[i]) / (self.__load + self.__loads[i])
 
 class Load(RTU):
 
     def __init__(self, **kwargs):
         super(Load, self).__init__(**kwargs)
-        if 'load' not in kwargs.keys() or not isinstance(kwargs['load'], float):
+        if any(k not in kwargs.keys() for k in ['load', 'left']):
+            raise AttributeError()
+        if not isinstance(kwargs['load'], float) or not isinstance(kwargs['left'], int):
             raise AttributeError()
         self.__load = kwargs['load']
+        self.__left = kwargs['left']
+        self.__vin = None
 
     @property
     def load(self) -> float:
         return self.__load
-
     @load.setter
     def load(self, new_load: float):
         self.__load = new_load
+    
+    @property
+    def left(self) -> int:
+        return self.__left
+    @left.setter
+    def left(self, value: int):
+        self.__left = value
 
     def __str__(self):
         return 'Load RTU\r\n-------------------\r\nID: {0:14d}\r\nLoad: {1:9.2f}'.format(self.guid, self.__load)
--- simcomm.py
+++ simcomm.py
@@ -2,6 +2,7 @@
 
 import sys
 import socket
+from time import sleep
 from struct import pack, unpack
 from threading import Thread
 from rtu import RTU_SOURCE, RTU_TRANSMISSION, RTU_LOAD, RTU_TYPES
@@ -37,14 +38,30 @@ class SimulationHandler(Thread):
         self.__sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)                       # Enable broadcast
         self.__sock.bind(('', SIM_PORT))
         self.__sock.settimeout(0.333)
+        self.__laddr = None
+        self.__raddr = None
     
     @property
     def terminate(self) -> bool:
         return self.__terminate
-    
     @terminate.setter
     def terminate(self, value: bool):
         self.__terminate = value
+    
+    def who_has_left(self):
+        count = 0
+        while not self.__terminate and self.__laddr is None:
+            try:
+                if (count % 10) == 0:
+                    data = pack(DATA_FMT, self.__rtu.guid, self.__rtu.left, MSG_WERE, 0, 0, 0.0, 0.0)
+                    self.__sock.sendto(data, ('<broadcast>', SIM_PORT))
+                data, addr = self.__sock.recvfrom(BUFFER_SIZE)
+                data = unpack(DATA_FMT, data)
+                if data[0] == self.__rtu.left and data[1] == self.__rtu.guid and data[2] == MSG_ISAT:
+                    self.__laddr = addr
+            except socket.timeout:
+                pass
+            count +=1
     
     def run_source(self):
         while not self.__terminate:
@@ -62,7 +79,15 @@ class SimulationHandler(Thread):
             except socket.timeout:
                 pass
     
+    def vin_polling(self):
+        while not self.__terminate:
+            data = pack(DATA_FMT, self.__rtu.guid, self.__rtu.left, MSG_GETV, 0, 0, 0.0, 0.0)
+            self.__sock.sendto(data, self.__laddr)
+            sleep(0.5)
+    
     def run_load(self):
+        vpoller = Thread(target=self.vin_polling)
+        vpoller.start()
         while not self.__terminate:
             try:
                 data, addr = self.__sock.recvfrom(BUFFER_SIZE)
@@ -72,13 +97,22 @@ class SimulationHandler(Thread):
                         data = pack(DATA_FMT, self.__rtu.guid, data[0], MSG_ISAT, 0, 0, 0.0, 0.0)
                     elif data[2] == MSG_GREQ:
                         data = pack(DATA_FMT, self.__rtu.guid, data[0], MSG_TREQ, 0, 0, self.__rtu.load, 0.0)
+                    elif data[2] == MSG_VOLT:
+                        self.__rtu.vin = data[5]
+                        data = None
                     else:
                         data = pack(DATA_FMT, self.__rtu.guid, data[0], MSG_UKWN, 0, 0, 0.0, 0.0)
+                    if data is not None:
+                        self.__sock.sendto(data, addr)
             except socket.timeout:
                 pass
+        vpoller.join()   
 
     def run(self):
-        if self.__rtu.rtutype == RTU_SOURCE and isinstance(self.__rtu, Source):
+        if self.__rtu.rtutype == RTU_SOURCE and self.__rtu.__class__.__name__ == 'Source':
             self.run_source()
+        if self.__rtu.rtutype == RTU_SOURCE and self.__rtu.__class__.__name__ == 'Load':
+            self.who_has_left()
+            self.run_load()
         else:
             raise ClassTypeMismatch()
